e1ac5538271e2e7ce328623d1efcf8bf523cb29f
michellebuchiokonicha <michelleokonicha.com>
2023-01-21 17:55:13 +0100

    u

---
+++ greet.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{if(argc == 2)
+{
+
+    printf("hello, %s\n", argv[1]);
+}
+else
+{
+    printf("hello, world\n");
+}
+    // string name = get_string("What is your name? ");
+    // printf("hello, %s %s\n", argv[1], argv[2]);
+}
