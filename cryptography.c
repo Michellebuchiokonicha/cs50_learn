abf3ddc7711063c56846bd53f7a865940bdf4a9f
michellebuchiokonicha <michelleokonicha.com>
2023-01-28 17:00:34 +0100

    u

--- cryptography.c
+++ cryptography.c
@@ -5,4 +5,17 @@ int main(void)
 {
     // this has to do with incription
     // a way of incripting things to mske it difficult for others except involved parties to decode.
+
+
+    // code for the total number of users in php
+
+       <?php
+                
+                                
+         $query = "SELECT id FROM users ORDER BY id";
+        $query_run = mysqli_query($connection, $query);
+
+        $row = mysqli_num_rows($query_run);
+        echo '<h1>'.$row.' </h1>'
+            ?> 
 }
