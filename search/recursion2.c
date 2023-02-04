93e4000e6fe0f586b6e301802fecf03ee3c390a6
michellebuchiokonicha <michelleokonicha.com>
2023-02-04 18:15:07 +0100

    u

---
+++ search/recursion2.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height = get_int("Height: ");
+}
+
+void draw(int n)
+{
+    draw(n -1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
